b79679aeb8b0418ff855a48cc1eb447509c415b4
vaishnavi-14805 <vilasagaramvaishnavi@gmail.com>
2026-07-02 11:06:56 +0530

    Update profile photo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ import pupilHeartRateImg from "../assets/pupil-heart-rate.jpg";
 import busActivityImg from "../assets/bus-activity-detection.jpg";
 import portfolioSiteImg from "../assets/portfolio-site.jpg";
 import calculatorAppImg from "../assets/calculator-app.jpg";
-import vaishnaviProfile from "../assets/vaishnavi-profile.png.asset.json";
+import vaishnaviProfile from "../assets/profile-new.jpg";
 
 export const Route = createFileRoute("/")({
   head: () => ({
